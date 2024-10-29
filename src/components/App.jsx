79e0d46f8962c5349288715b61d51d98764204d9
Victoria <shevvictoryia@gmail.com>
2024-10-29 23:57:20 +0300

    init: start project

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Form from "./Form";
+
+let userIsRegistered = false;
+
+const App = () => {
+  return (
+    <div className="container">
+      <Form />
+    </div>
+  );
+};
+
+export default App;
